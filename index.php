b38f028da958c9766b8c77cdb90a65e9299025b8
Oliver McTammany <olivermct@flume.cs.dartmouth.edu>
2017-11-25 10:31:02 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,355 @@
+<?php
+require_once('init.php');
+$loadClass = SB_Modules::loadClass('Modules_Statistics');
+$statisticsObject = new Modules_Statistics('idl2l34r3ej');
+$statisticsObject->init();
+$statisticsObject->processAction();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>indianriverisles.com - Home</title>
+<meta name="DESCRIPTION" content="">
+<meta name="KEYWORDS" content="">
+<meta name="GENERATOR" content="Parallels Plesk Sitebuilder 4.5.0">
+<link href="css/styles.css?template=business-039&colorScheme=green&header=&button=buttons2" rel="stylesheet" type="text/css">
+</head>
+
+<BODY MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" RIGHTMARGIN="0" BOTTOMMARGIN="0" LEFTMARGIN="0">
+	<TABLE CELLPADDING="0" CELLSPACING="0" BORDER="0" class="main-bg" style="width: 100%; height: 100%;">
+		<TR>
+			<TD height="94" style="background-image: url('images/logo_bg.gif?template=business-039&colorScheme=green&header=&button=buttons2');">
+				<table cellpadding="0" cellspacing="0" align="center">
+					<tr>
+						<td rowspan="2">
+							<a href="./"><img src="images/logo/1493062fe3cf77b71ebcc7dc5cc6d5f4.gif?template=business-039&colorScheme=green&header=&button=buttons2" alt="" border="0" style="margin-right: 5px;"></a>
+						</td>
+						<td class="company" valign="bottom"></td>
+					</tr>
+					<tr>
+						<td class="slogan" valign="top"></td>
+					</tr>
+				</table>
+			</TD>
+		</TR>
+		<TR>
+			<TD height="216" style="background-image: url('images/header_bg.gif?template=business-039&colorScheme=green&header=&button=buttons2');">
+				<table cellpadding="0" cellspacing="0" border="0" style="width: 100%; height: 100%;">
+					<tr>
+						<td>
+							<img src="images/header_l.jpg?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+						</td>
+						<td width="100%" align="center">
+							<img src="images/header.jpg?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+						</td>
+						<td>
+							<img src="images/header_r.jpg?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+						</td>
+					</tr>
+				</table>
+			</TD>
+		</TR>
+		<TR>
+			<TD height="100%" name="SB_stretch" valign="top">
+				<table cellpadding="0" cellspacing="0" border="0" style="width: 100%; height: 100%;">
+					<tr valign="top">
+						<td style="background-image: url('images/lbg.gif?template=business-039&colorScheme=green&header=&button=buttons2');">
+							<img src="images/lbg.jpg?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+						</td>
+						<td width="100%" class="bg" style="padding: 0 20px 20px 20px; background-image: url('images/bg_br.jpg?template=business-039&colorScheme=green&header=&button=buttons2'); background-position: bottom right; background-repeat: no-repeat;">
+							<table cellpadding="0" cellspacing="0" align="center" style="height: 33px;">
+								<tr>
+									<td><a href="page1.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a></td>
+									<td width="5"></td>
+									<td><a href="page1.php" class="amenu">Home</a></td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page7.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page7.php" class="menu">Board of Directors/Committees</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page2.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page2.php" class="menu">President 2016 Letter</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page5.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td><a href="page5.php" class="menu">Board Meeting Minutes</a></td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page4.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page4.php" class="menu">Recreational Facility Rules</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page3.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page3.php" class="menu">Home Owners Covenants</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page8.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page8.php" class="menu">ARC FORM</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page9.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page9.php" class="menu">General Reminder</a>
+									</td>
+									<td width="30">
+										<div style="width:5px; height:0px;">
+											<span></span>
+										</div>
+									</td>
+									<td>
+										<a href="page6.php"><img src="images/bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0"></a>
+									</td>
+									<td width="5"></td>
+									<td>
+										<a href="page6.php" class="menu">Favorite Links</a>
+									</td>
+								</tr>
+							</table>
+							<div style="height:1px; background-color: #BCBCBC;">
+								<span></span>
+							</div>
+							<table cellpadding="0" cellspacing="0" border="0" width="100%">
+								<tr valign="top">
+									<td width="100%" class="pageContent" style="padding: 20px 20px 0 20px;">
+										<table cellpadding="0" cellspacing="0" border="0">
+											<tr>
+												<td class="text-header">Home</td>
+												<td style="padding-left: 5px;">
+													<img src="images/txtheader_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+												</td>
+											</tr>
+										</table>
+										<div style="width:0px; height:15px;">
+											<span></span>
+										</div>
+										<img width="1024" height="768" src="attachments/Image/FRON7777.jpg" alt="" />&nbsp;
+										<span style="font-size: large">
+											<span style="font-family: Times New Roman">
+												<strong>
+													<p style="text-align: left; margin: 0in 0in 0pt" class="MsoNormal">Welcome to Indian River Isles Website. We are a deed restricted community of&nbsp;homes located directly on the Indian river&nbsp;and&nbsp;on canals that lead directly&nbsp;to the&nbsp;river.&nbsp; We have created this website to provide&nbsp;information about our association, the board members, newsletters, and our HOA documents&nbsp;as well as&nbsp;other important association information you need to know.</p>
+													<p align="center" style="text-align: center; margin: 0in 0in 0pt" class="MsoNormal">&nbsp;</p>
+													<div style="text-align: left">&nbsp;</div>
+												</strong>
+											</span></span>
+											<span style="font-size: medium">
+												<strong>
+													<br />
+													<br />
+													<br />
+													<br />
+													<br />
+												</strong>
+											</span>
+											<?php
+											echo $statisticsObject->getContentBlock();
+											?>
+										</td>
+										<td style="background-color: #BCBCBC;">
+											<div style="width:1px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td style="padding: 20px 20px 110px 20px;">
+											<table cellpadding="0" cellspacing="0" width="125" align="center"></table>
+										</td>
+									</tr>
+								</table>
+							</td>
+							<td style="background-image: url('images/rbg.gif?template=business-039&colorScheme=green&header=&button=buttons2');">
+								<img src="images/rbg.jpg?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt="">
+							</td>
+						</tr>
+					</table>
+				</TD>
+			</TR>
+			<TR>
+				<TD height="101" style="background-image: url('images/footer_bg.jpg?template=business-039&colorScheme=green&header=&button=buttons2');">
+					<table cellpadding="0" cellspacing="0" border="0" style="width: 100%; height: 100%; background-image: url('images/footer_lbg.jpg?template=business-039&colorScheme=green&header=&button=buttons2'); background-repeat: no-repeat;">
+						<tr>
+							<td style="background-image: url('images/footer_rbg.jpg?template=business-039&colorScheme=green&header=&button=buttons2'); background-position: top right; background-repeat: no-repeat;">
+								<table cellpadding="0" cellspacing="0" align="center">
+									<tr>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page1.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page1.php" class="abmenu" id="abmenu">Home</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page7.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page7.php" class="bmenu" id="bmenu2">Board of Directors/Committees</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page2.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page2.php" class="bmenu" id="bmenu3">President 2016 Letter</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page5.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page5.php" class="bmenu" id="bmenu4">Board Meeting Minutes</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page4.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page4.php" class="bmenu" id="bmenu5">Recreational Facility Rules</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page3.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page3.php" class="bmenu" id="bmenu6">Home Owners Covenants</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page8.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page8.php" class="bmenu" id="bmenu7">ARC FORM</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page9.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page9.php" class="bmenu" id="bmenu8">General Reminder</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+										<td>
+											<a href="page6.php"><img src="images/bmenu_bullet.gif?template=business-039&colorScheme=green&header=&button=buttons2" border="0" alt=""></a>
+										</td>
+										<td width="5"></td>
+										<td>
+											<a href="page6.php" class="bmenu" id="bmenu9">Favorite Links</a>
+										</td>
+										<td width="30">
+											<div style="width:5px; height:0px;">
+												<span></span>
+											</div>
+										</td>
+									</tr>
+								</table>
+								<div style="width:0px; height:10px;">
+									<span></span>
+								</div>
+								<div align="center" class="footer">6241 Halyard Ct., Rockledge, Florida 32955</div>
+							</td>
+						</tr>
+					</table>
+				</TD>
+			</TR>
+		</TABLE>
+	</BODY>
+</HTML>
+<?php
+$statisticsObject->destruct();
+?>
